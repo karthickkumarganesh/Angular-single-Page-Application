9340cea3cc2e03b2914b477750ca56e6a750fdec
karthick kumar <karthickkumarganesh@gmail.com>
2015-04-22 12:01:50 +0530

    removed log prints
    removed log prints

--- js/controller.js
+++ js/controller.js
@@ -5,14 +5,14 @@ var singlePageControllers=angular.module('singlePageControllers',[]);
 
 singlePageControllers.controller('ListController',['$scope','$http',function($scope,$http){
 	$http.get("mydata.php").success(function(data){
-		console.log(data);
+		
 		$scope.users=data;		
 	});
 }]);
 
 singlePageControllers.controller('ViewController',['$scope','$http','$routeParams',function($scope,$http,$routeParams){
 	$http.get("getdatasingle.php?userid="+$routeParams.userid).success(function(data){
-		console.log(data);
+		
 		$scope.usersdetail=data;		
 	});
 }]);
